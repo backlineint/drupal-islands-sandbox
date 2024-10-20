b357c5e57b25c1268b781e6d2502e9692b57c248
Brian Perry <contact.brianperry@gmail.com>
2024-10-20 16:16:55 -0500

    Update islands module to use importmaps

--- web/modules/custom/islands_examples/templates/is-land-theme.island.js
+++ web/modules/custom/islands_examples/templates/is-land-theme.island.js
@@ -1,5 +1,6 @@
 import { html, render } from "https://unpkg.com/htm/preact/index.mjs?module";
 
+// TODO - update this to the classic counter example
 function App(props) {
   return html`<p><strong>Hello ${props.name}!</strong></p>`;
 }
